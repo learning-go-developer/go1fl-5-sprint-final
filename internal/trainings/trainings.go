91eec94eae8d78ba11fb5db629c2a8ad34543feb
my-app-s <94853425+my-app-s@users.noreply.github.com>
2026-02-28 16:14:16 -0500

    Fix imports: add time and correct personaldata path

--- internal/trainings/trainings.go
+++ internal/trainings/trainings.go
@@ -1,7 +1,9 @@
 package trainings
 
 import (
-	"internal/personaldata"
+	"time"
+
+	"github.com/Yandex-Practicum/tracker/internal/personaldata"
 )
 
 // Training represents a workout session.
@@ -11,10 +13,10 @@ import (
 // name, weight, height, and inherits the Print() method for displaying
 // this information.
 type Training struct {
-	Steps 					int				// number of steps during training
-	TrainingType 			string			// type of training: "running" or "walking"
-	Duration 				time.Duration	// duration of the workout
-	personaldata.Personal					// embedded struct with Name, Weight, Height
+	Steps                 int           // number of steps during training
+	TrainingType          string        // type of training: "running" or "walking"
+	Duration              time.Duration // duration of the workout
+	personaldata.Personal               // embedded struct with Name, Weight, Height
 }
 
 func (t *Training) Parse(datastring string) (err error) {
