62a7326b331f130e782a0ab762ed3c1a08041755
my-app-s <94853425+my-app-s@users.noreply.github.com>
2026-02-28 16:06:44 -0500

    Add Training struct with embedded Personal

--- internal/trainings/trainings.go
+++ internal/trainings/trainings.go
@@ -1,7 +1,20 @@
 package trainings
 
+import (
+	"internal/personaldata"
+)
+
+// Training represents a workout session.
+// It contains the number of steps, the type of training (running or walking),
+// the duration of the workout, and embedded personal data about the user.
+// By embedding personaldata.Personal, Training also includes the user's
+// name, weight, height, and inherits the Print() method for displaying
+// this information.
 type Training struct {
-	// TODO: добавить поля
+	Steps 					int				// number of steps during training
+	TrainingType 			string			// type of training: "running" or "walking"
+	Duration 				time.Duration	// duration of the workout
+	personaldata.Personal					// embedded struct with Name, Weight, Height
 }
 
 func (t *Training) Parse(datastring string) (err error) {
