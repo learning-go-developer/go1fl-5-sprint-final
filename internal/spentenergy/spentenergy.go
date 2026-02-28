de091e5706425255a93abd9a15987d0056fff345
my-app-s <94853425+my-app-s@users.noreply.github.com>
2026-02-28 12:42:09 -0500

    Implement Distance function

--- internal/spentenergy/spentenergy.go
+++ internal/spentenergy/spentenergy.go
@@ -24,6 +24,12 @@ func MeanSpeed(steps int, height float64, duration time.Duration) float64 {
 	// TODO: реализовать функцию
 }
 
+// Distance calculates the traveled distance in kilometers
+// based on the number of steps and the person's height.
+// It uses a step length coefficient and converts meters to kilometers.
 func Distance(steps int, height float64) float64 {
-	// TODO: реализовать функцию
+	stepLength := height * stepLengthCoefficient
+	distanceMeters := float64(steps) * stepLength
+	
+	return distanceMeters / mInKm
 }
