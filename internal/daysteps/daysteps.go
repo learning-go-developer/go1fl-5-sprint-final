bf6097cbfe0343c2aef06dba0a393d179258bf53
my-app-s <94853425+my-app-s@users.noreply.github.com>
2026-03-01 02:54:41 -0500

    Add DaySteps struct with embedded Personal

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -1,7 +1,21 @@
 package daysteps
 
+import (
+	"github.com/Yandex-Practicum/tracker/internal/personaldata"
+)
+
+// DaySteps represents a record of daily physical activity.
+// It stores the number of steps taken, the total duration of activity,
+// and personal information about the user.
+//
+// Fields:
+// - Steps: the number of steps completed during the day.
+// - Duration: the total time spent being physically active.
+// - Personal: embedded personal data of the user (e.g., name, age).
 type DaySteps struct {
-	// TODO: добавить поля
+	Steps int
+	Duration time.Duration
+	personaldata.Personal
 }
 
 func (ds *DaySteps) Parse(datastring string) (err error) {
