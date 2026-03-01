b9575851c6158219c9c21f4fe7f99943f1af75c6
my-app-s <94853425+my-app-s@users.noreply.github.com>
2026-03-01 03:42:42 -0500

    feat: add DataParser interface with Parse and ActionInfo methods

--- internal/actioninfo/actioninfo.go
+++ internal/actioninfo/actioninfo.go
@@ -1,7 +1,15 @@
 package actioninfo
 
+// DataParser defines the behavior for types that can parse input
+// data and provide a formatted activity summary.
+//
+// Implementations of this interface must provide:
+//   - Parse(): to process and validate input data;
+//   - ActionInfo(): to generate a formatted string with details
+//     about the activity (e.g., steps, distance, calories).
 type DataParser interface {
-	// TODO: добавить методы
+	Parse()
+	ActionInfo()
 }
 
 func Info(dataset []string, dp DataParser) {
